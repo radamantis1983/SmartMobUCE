22bad625c5739aafdaf5e32ac074bb6c38e5386f
radamantis1983 <37030669+radamantis1983@users.noreply.github.com>
2018-04-01 16:34:33 -0500

    actualizacion de imagenes

--- app/src/main/java/ec/edu/uce/smartmobuce/vista/GPSActivity.java
+++ app/src/main/java/ec/edu/uce/smartmobuce/vista/GPSActivity.java
@@ -7,12 +7,13 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
-import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
