14a2affc2e1c3dfa6d12e9d64dbbc6ea2a129f18
radamantis1983 <37030669+radamantis1983@users.noreply.github.com>
2018-07-23 01:21:33 -0500

    agregando ejecucion automatica en caso de apagar el dispositivo al momento de reiniciar o encender el equipo el servicio se ejecutara automaticamente

---
+++ app/src/main/java/ec/edu/uce/smartmobuce/controlador/EjecucionAutomatica.java
@@ -0,0 +1,19 @@
+package ec.edu.uce.smartmobuce.controlador;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Henry on 7/23/2018.
+ */
+
+public class EjecucionAutomatica extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent service = new Intent(context,  GpsService.class);
+        context.startService(service);
+
+    }
+}
